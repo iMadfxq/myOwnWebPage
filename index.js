4fdbd305d5b5fae3ff3ac5d292877456c7a518ba
iMadfxq <imadfxq@gmail.com>
2022-03-29 22:34:34 -0500

    Added professional e-mail. Im using Prettier to format my code.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const documentBody = document.querySelector('body')
-const showButton = document.querySelector('#samples--display')
-const regularSamples = document.querySelector('.samples')
-const hiddenSamples = document.querySelector('#samples--hidden')
+const documentBody = document.querySelector("body");
+const showButton = document.querySelector("#samples--display");
+const regularSamples = document.querySelector(".samples");
+const hiddenSamples = document.querySelector("#samples--hidden");
 
-documentBody.addEventListener('click', (e) => {
-  e.stopPropagation()
-  if(e.target === showButton && hiddenSamples.style.display == 'none') {
-    regularSamples.style.display = 'none'
-    hiddenSamples.style.display = 'flex'
-    hiddenSamples.scrollIntoView(true, {behavior: 'smooth'})
-    showButton.textContent = '- Show less'
-  } else if(e.target === showButton && hiddenSamples.style.display == 'flex'){
-    regularSamples.style.display = 'flex'
+documentBody.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target === showButton && hiddenSamples.style.display == "none") {
+    regularSamples.style.display = "none";
+    hiddenSamples.style.display = "flex";
+    hiddenSamples.scrollIntoView(true, { behavior: "smooth" });
+    showButton.textContent = "- Show less";
+  } else if (e.target === showButton && hiddenSamples.style.display == "flex") {
+    regularSamples.style.display = "flex";
     setTimeout(() => {
-      hiddenSamples.style.display = 'none'
-    }, 550) //to avoid it showing the bottom of the webpage before scrollingIntoView 
-    regularSamples.scrollIntoView(true, {behavior: 'smooth'})
-    showButton.textContent = '+ Show more'
+      hiddenSamples.style.display = "none";
+    }, 550); //to avoid it showing the bottom of the webpage before scrollingIntoView
+    regularSamples.scrollIntoView(true, { behavior: "smooth" });
+    showButton.textContent = "+ Show more";
   }
-})
+});
