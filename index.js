8c1f96058d74781967c03fd88dad6395584d625c
iMadfxq <imadfxq@gmail.com>
2022-04-06 10:24:39 -0500

    Added meta description to improve SEO

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ documentBody.addEventListener("click", (e) => {
   if (e.target === showButton && hiddenSamples.style.display == "none") {
     regularSamples.style.display = "none";
     hiddenSamples.style.display = "flex";
-    hiddenSamples.scrollIntoView(true, { behavior: "smooth" });
+    document.querySelector('#projectsScrollIntoView').scrollIntoView(true, { behavior: "smooth" });
     showButton.textContent = "- Show less";
   } else if (e.target === showButton && hiddenSamples.style.display == "flex") {
     regularSamples.style.display = "flex";
     setTimeout(() => {
       hiddenSamples.style.display = "none";
     }, 550); //to avoid it showing the bottom of the webpage before scrollingIntoView
-    regularSamples.scrollIntoView(true, { behavior: "smooth" });
+    document.querySelector('#projectsScrollIntoView').scrollIntoView(true, { behavior: "smooth" });
     showButton.textContent = "+ Show more";
   }
 });
